b5d88100417cd56b72e371f9a0a9dd5bbbcdf1c0
edrees <muhammed.edrees101@gmail.com>
2025-02-12 01:24:50 +0200

    Solver merge conflict

--- common/src/main/java/com/zaed/common/data/model/User.kt
+++ common/src/main/java/com/zaed/common/data/model/User.kt
@@ -4,7 +4,6 @@ data class User(
     val id : String = "",
     val fullName :String = "",
     val userName: String = "",
-    val fullName: String = "",
     val password: String = "",
     val approvementStatusType: UserApprovementStatusType = UserApprovementStatusType.PENDING,
     val role: UserRole = UserRole.NONE
