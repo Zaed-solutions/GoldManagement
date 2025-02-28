7bf3ac139fabd6e4b05e14c16c2ad5e62968c7a0
edrees <muhammed.edrees101@gmail.com>
2025-02-28 06:46:08 +0200

    feat: Manage inventory in store and wholesale sales
    - Added inventory management for store sales.
    - Added inventory management for wholesale sales.
    - Added inventory management for Ingot transactions.
    - Introduced `Inventory` model and `InventoryType` enum for tracking inventory.
    - Store sale and delete updates now modify the store's inventory accordingly.
    - Wholesale gold and product sales now manage customer's gold and product inventory.
    - Changed IngotTransaction's update method to manage inventory.

---
+++ common/src/main/java/com/zaed/common/data/model/Inventory.kt
@@ -0,0 +1,19 @@
+package com.zaed.common.data.model
+
+import com.zaed.common.data.model.sale.Karat
+import java.util.Date
+
+data class Inventory(
+    val id: String = "",
+    val productId: String = "",
+    val quantity: Double = 0.0,
+    val lastUpdated: Date = Date(),
+    val type: InventoryType,
+    val karat: String = "",
+)
+
+enum class InventoryType {
+    PRODUCT,
+    GOLD,
+    INGOT
+}
