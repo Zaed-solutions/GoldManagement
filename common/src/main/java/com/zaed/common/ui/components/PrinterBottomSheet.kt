ba2f089a7f49ea11c46cc6d1d19f367dd9d2a0a1
edrees <muhammed.edrees101@gmail.com>
2025-03-18 02:22:51 +0200

    Refactor: Update device selection in PrinterBottomSheet
    - Changed the background color of the device selection button to transparent.

--- common/src/main/java/com/zaed/common/ui/components/PrinterBottomSheet.kt
+++ common/src/main/java/com/zaed/common/ui/components/PrinterBottomSheet.kt
@@ -43,6 +43,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -278,7 +279,8 @@ fun PrinterBottomSheet(
                                             selectedDeviceName =
                                                 device.device.name ?: "Unknown device"
                                             showDeviceDialog = false
-                                        }
+                                        },
+                                        color = Color.Transparent
                                     ) {
                                         Row(
                                             modifier = Modifier
